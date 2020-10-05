ef3536a3e0a791588eb70919ae268ebb28d27506
Remo <43846137+RemGal@users.noreply.github.com>
2020-10-05 17:00:55 +0200

    temporarily added library functionality

---
+++ temp/vector.js
@@ -0,0 +1,127 @@
+/**
+ * @name VectorJS
+ * @description An implementation of two dimensional mathematical vectors in JavaScript.
+ * @author Remigiusz Galikowski
+ */
+
+class Vector {
+    /**
+    * A vector is defined as a pair of X and Y coordinates with magnitude and direction.
+    * Each operational method can accept either a Vector object or X and Y coordinates.
+    *
+    * @param {x} x
+    * @param {y} y
+    */
+    constructor(x,y) {
+        this._x = x;
+        this._y = y;
+    }
+  
+    set x(newX) {
+        this._x = newX;
+    }
+    get x() {
+        return this._x;
+    }
+  
+    set y(newY) {
+        this._y = newY;
+    }
+    get y() {
+        return this._y;
+    }
+
+    /**
+    * length() is implemented as a getter, so you can just call 'vector.length'
+    * calculates the magnitude of the vector based on the following equation:
+    * length^2 = x^2 + y^2
+    */
+    get length() {
+        let x = this._x, y = this._y;
+        return Math.sqrt(x * x + y * y);
+    }
+  
+    /**
+    * adds two vectors
+    * vector.add(vector) or vector.add(x,y)
+    * @param {(number|Object)} arg1 - x or Vector object.
+    * @param {(number|undefined)} arg2 - y or blank.
+    */
+    add(arg1,arg2) {
+        if(arg1 instanceof Vector && typeof arg2 === 'undefined') {
+            this._x += arg1.x;
+            this._y += arg1.y;
+         }
+        else if (typeof arg1 === 'number' && typeof arg1 === 'number') {
+             this._x += arg1;
+             this._y += arg2;
+         }
+         else throw new TypeError('Wrong type of arguments in vector.add().');
+    }
+    
+    /**
+    * subtracts two vectors
+    * vector.subtract(vector) or vector.subtract(x,y)
+    * @param {(number|Object)} arg1 - x or Vector object.
+    * @param {(number|undefined)} arg2 - y or blank.
+    */
+    subtract(arg1,arg2) {
+        if(arg1 instanceof Vector && typeof arg2 === 'undefined') {
+            this._x -= arg1.x;
+            this._y -= arg1.y;
+         }
+        else if (typeof arg1 === 'number' && typeof arg1 === 'number') {
+             this._x -= arg1;
+             this._y -= arg2;
+         }
+         else throw new TypeError('Wrong type of arguments in vector.subtract().');
+    }
+    
+    /**
+    * multiplies two vectors
+    * vector.multiply(vector) or vector.multiply(x,y)
+    * @param {(number|Object)} arg1 - x or Vector object.
+    * @param {(number|undefined)} arg2 - y or blank.
+    */
+    multiply(arg1,arg2) {
+        if(arg1 instanceof Vector && typeof arg2 === 'undefined') {
+            this._x *= arg1.x;
+            this._y *= arg1.y;
+         }
+        else if (typeof arg1 === 'number' && typeof arg1 === 'number') {
+             this._x *= arg1;
+             this._y *= arg2;
+         }
+         else throw new TypeError('Wrong type of arguments in vector.multiply().');
+    }
+    
+    /**
+    * divides two vectors
+    * vector.divide(vector) or vector.divide(x,y)
+    * @param {(number|Object)} arg1 - x or Vector object.
+    * @param {(number|undefined)} arg2 - y or blank.
+    */
+    divide(arg1,arg2) {
+        if(arg1 instanceof Vector && typeof arg2 === 'undefined') {
+            this._x /= arg1.x;
+            this._y /= arg1.y;
+         }
+        else if (typeof arg1 === 'number' && typeof arg1 === 'number') {
+             this._x /= arg1;
+             this._y /= arg2;
+         }
+         else throw new TypeError('Wrong type of arguments in vector.divide().');
+    }
+
+    /**
+    * A method to normalize a vector.
+    */
+    normalize() {
+        if (this._x != 0 && this._y != 0) {
+            let mag = this.length;
+            this._x /= mag;
+            this._y /= mag; 
+        }
+    }
+
+}
